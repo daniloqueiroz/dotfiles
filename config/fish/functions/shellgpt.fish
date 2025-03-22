b8f1394a86d1e6e7ea2a4341103c8d345b02902d
danilo queiroz <dpenna.queiroz@gmail.com>
2025-03-22 13:40:54 -0400

    WIP

--- config/fish/functions/shellgpt.fish
+++
@@ -1,7 +0,0 @@
-function shellgpt -d "Ask gpt to execute shell operations"
-  set -l cmd (commandline -b)
-  commandline -f kill-whole-line
-  echo -en "\ngpt > "
-  sgpt --shell "$cmd"
-  commandline -f repaint
-end
